f3f6da0b41f207a6c245c3c13f294336853d16a3
tpwonder <97324746+tpwonder@users.noreply.github.com>
2022-02-06 22:10:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,21 +14,14 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 ###### Modify hostname
-sed -i 's/OpenWrt/K2P/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Newifi D1/g' package/base-files/files/bin/config_generate
 
 ####### Modify the version number
-sed -i "s/OpenWrt /复工快乐！Steven build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /Steven build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-###### K2P 32M Support
-sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
-sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<80000000>/<10000000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/m25p,fast-read;/broken-flash-reset;/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-
-###### Set argon as default theme
+####### Set argon as default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-###### Replace with JerryKuKu’s Argon
+####### Replace with JerryKuKu’s Argon
 rm openwrt/package/lean/luci-theme-argon -rf #delete original argon theme
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  #repleace with JerryKuKu’s Argon
