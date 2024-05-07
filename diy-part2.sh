54157290bea230b6acf3c8de086fd5fe1c4be524
tpwonder <97324746+tpwonder@users.noreply.github.com>
2024-05-08 07:20:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.30/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/NewifiD1/g' package/base-files/files/bin/config_generate
