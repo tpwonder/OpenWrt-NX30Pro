3d2824059bd95bc0af8d00c006bdf578b26474f5
tpwonder <97324746+tpwonder@users.noreply.github.com>
2024-05-08 07:21:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,10 @@
 sed -i 's/192.168.1.1/192.168.1.30/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/NewifiD1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/NX30Pro/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
 sed -i "s/OpenWrt /Steven build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Set argon as default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
